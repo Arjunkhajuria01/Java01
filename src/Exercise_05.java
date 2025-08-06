2dfe6bc6b051932667e816360037d91ab3004249
Arjunkhajuria01 <bharatsaida67@gmail.com>
2025-08-06 22:50:58 +0530

    Updating Java Progress

---
+++ src/Exercise_05.java
@@ -0,0 +1,70 @@
+
+
+import java.util.Scanner;
+// loops questions
+public class Exercise_05 {
+    public static void main(String[] args) {
+        // q) write a program to print the following pattern :-
+//         ****
+//         ***
+//         *
+//         *
+//        int n = 1;
+//        for(int i=4; i>=n ; i--) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // (q2) SUM OF FIRST N EVEN NUMBERS USING WHILE LOOP
+//        System.out.println("SUM OF FIRST 10 EVEN NUMBERS USING WHILE LOOP");
+//        int i = 0 ;
+//
+//
+//        int k= 0 ;
+//            while (i <= 20) {
+//
+//                int j = 2 * i;
+//                System.out.println(j);
+//
+//                k += j;
+//
+//                i++;
+//
+//
+//            }
+//        System.out.println(k);
+        // q 3)
+        // Write a program to print a multiplication table fo give number
+
+//        System.out.println("MULTIPLICATION TABLE");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the number");
+//        int a = sc.nextInt();
+//        for (int b = 0 ; b<= 10 ; b++){
+//            int x = a*b;
+//            System.out.println(a + "X" + b + "=" + x);
+//        }
+
+        // q 4) print multiplication table of 10 in reverse
+
+//        System.out.println("MULTIPLICATION TABLE");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the number");
+//        int a = sc.nextInt();
+//        for (int b = 10 ; b>= 0 ; b--){
+//            int x = a*b;
+//            System.out.println(a + "X" + b + "=" + x);
+//        }
+
+        // Write a program to find factorial of a given number using for loop
+
+        System.out.println("FACTORIAL");
+
+
+        }
+
+        }
+
+
