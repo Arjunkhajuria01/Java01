52a3c815cd5bf06e31b7054ae48d952554f32571
Arjunkhajuria01 <bharatsaida67@gmail.com>
2025-09-29 23:48:28 +0530

    update

--- src/Multi_Dim_Array_21.java
+++ src/Multi_Dim_Array_21.java
@@ -1,28 +1,35 @@
 // Multidimensional arrays
 public class Multi_Dim_Array_21 {
     public static void main(String[] args) {
-        //Multidimensional Arrays are aarays of arrray ;
-        // Each element of of a MUlti_D array is an array itself
-        int[]marks ; //1 d array
-        int[][]marks2; //2d array
-        marks2 = new int[2][3];  // this means 2 rows and three columns
-        marks2[0][0] = 10;
-        marks2[0][1] = 32;
-        marks2[0][2] = 54;
-        marks2[1][0] = 23;
-        marks2[1][1] = 67;
-        marks2[1][2] = 21;
-
-        // Printing a 2-D Array using for loops
-        for(int i= 0; i< marks2.length ; i++) {
-            for (int j = 0; j <marks2[i].length; j++) {
-                System.out.print(marks2[i][j]);
-                System.out.print("  ");
-
-            }
-            System.out.println();
-        }
-
+//        //Multidimensional Arrays are aarays of arrray ;
+//        // Each element of of a MUlti_D array is an array itself
+//        int[]marks ; //1 d array
+//        int[][]marks2; //2d array
+//        marks2 = new int[3][3];  // this means 2 rows and three columns
+//        marks2[0][0] = 10;
+//        marks2[0][1] = 32;
+//        marks2[0][2] = 54;
+//        marks2[1][0] = 23;
+//        marks2[1][1] = 67;
+//        marks2[1][2] = 21;
+//        marks2[2][0] = 43;
+//        marks2[2][1] = 72;
+//        marks2[2][2] = 20;
+//
+//
+//
+//        // Printing a 2-D Array using for loops
+//        for(int i= 0; i< marks2.length ; i++) {
+//            for (int j = 0; j <marks2.length; j++) {
+//                System.out.print(marks2[i][j]);
+//                System.out.print("  ");
+//
+//            }
+//            System.out.println();
+//        }
+int a = 100;
+String b = "" + a;
+        System.out.println(b.getClass());
 
     }
 }
---
+++ src/Recursion.java
@@ -0,0 +1,12 @@
+// RECURSION
+// A function in java can call itself , such calling of function is called recursion.
+
+
+
+
+
+public class Recursion {
+    public static void main(String[] args) {
+
+    }
+}
